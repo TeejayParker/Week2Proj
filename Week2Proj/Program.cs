aed61da9a8d3aac1d5cf1ac8e208feef5b6a3744
Teejay <teejayparker@gmail.com>
2018-11-12 19:45:23 -0600

    Add project files.

---
+++ Week2Proj/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System;
+
+namespace WelcomeApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Define Variables
+            string softwareDeveloperName = "Teejay";
+            string streetAddress = "123 Main St Anytown, USA";
+            string grossMonthlyPay = "$3,525.00";
+            string payTaxes = "$575.00";
+
+            //Write to Console
+            Console.WriteLine("Name: " + softwareDeveloperName);
+            Console.WriteLine("Street Address: " + streetAddress);
+            Console.WriteLine("grossMonthlyPay: " + grossMonthlyPay);
+            Console.WriteLine("Taxes: " + payTaxes);
+
+            //Show Console
+            Console.ReadLine();
+        }
+    }
+}
